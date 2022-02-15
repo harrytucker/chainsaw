1b4b7bfc22405e3be820160b56cbd1df92eb801d
Harry Tucker <harry.tucker@hpe.com>
2022-02-15 15:39:19 +0000

    Fix Clippy lint by moving logging mod into file

--- chainsaw-demo/src/logging.rs
+++ chainsaw-demo/src/logging.rs
@@ -1,3 +1,9 @@
+//! Structured logging gives us the ability to more easily programmatically
+//! interact with logs produced by applications. The most widely used crate for
+//! structured logging is the [`tracing`] crate, so this module provides some
+//! opinionated configurations to allow you to use [`tracing`] in your
+//! microservice.
+
 use crate::Result;
 
 use tracing::{subscriber::set_global_default, Level, Subscriber};
