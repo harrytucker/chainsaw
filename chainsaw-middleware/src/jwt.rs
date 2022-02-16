e53057928ff4ad84504bcb071d6c9fb7a09d5d58
mattdowdell <mdowdell244@gmail.com>
2022-02-16 11:25:58 +0000

    add UT + doc test

--- chainsaw-middleware/src/jwt.rs
+++ chainsaw-middleware/src/jwt.rs
@@ -41,9 +41,40 @@ pub struct Claims {
 ///
 /// This performs zero validation upon the JWT for the signature, expiry, etc. as all such concerns
 /// are beyond the scope of microservices and are instead deferred to Istio and Authz.
+///
+/// # Example
+/// ```
+/// # use chainsaw_middleware::jwt::JWTError;
+/// use chainsaw_middleware::jwt::decode_jwt;
+///
+/// # fn example() -> Result<(), JWTError> {
+/// let claims = decode_jwt("an.example.jwt")?;
+///
+/// println!("user id:     {}", claims.sub);
+/// println!("customer id: {}", claims.user_ctx);
+/// # Ok(())
+/// # }
+/// ```
 pub fn decode_jwt<S: AsRef<str>>(token: S) -> Result<Claims, JWTError> {
     let token = token.as_ref();
     let data = jsonwebtoken::decode::<Claims>(token, &DECODING_KEY, &VALIDATION)?;
 
     Ok(data.claims)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_JWT: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuY29tIiwiYXVkIjoiYXVkIiwic3ViIjoiZXhhbXBsZUBleGFtcGxlLmNvbSIsInVzZXJfY3R4IjoiODkyMmFmYTY3MjMwMTFlYmJlMDFjYTMyZDMyYjZiNzciLCJpYXQiOjE2MTU5MTAzOTksImV4cCI6MTYxNTkxNzU5OX0.KGVrRxlehq_h6qmtGXM-i-2IKJbMRqrmSj6FDEXaHEo";
+
+    #[test]
+    fn test_decode_jwt() -> Result<(), JWTError> {
+        let claims = decode_jwt(EXAMPLE_JWT)?;
+
+        assert_eq!(claims.sub, "example@example.com");
+        assert_eq!(claims.user_ctx, "8922afa6723011ebbe01ca32d32b6b77");
+
+        Ok(())
+    }
 }
