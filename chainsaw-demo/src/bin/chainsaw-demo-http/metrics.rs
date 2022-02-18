b1d6b989679ea063c2446f7d2ae7dec7efb3229a
Harry Tucker <harry.tucker@hpe.com>
2022-02-18 11:02:09 +0000

    Simplify fn report_metrics by skipping buffer

--- chainsaw-demo/src/bin/chainsaw-demo-http/metrics.rs
+++ chainsaw-demo/src/bin/chainsaw-demo-http/metrics.rs
@@ -26,14 +26,9 @@ pub async fn report_metrics(
     Extension(metrics_registry): Extension<Registry>,
 ) -> Result<String, StatusCode> {
     // Gather the metrics.
-    let mut buffer = vec![];
     let encoder = TextEncoder::new();
     let metric_families = metrics_registry.gather();
-    if encoder.encode(&metric_families, &mut buffer).is_err() {
-        return Err(StatusCode::INTERNAL_SERVER_ERROR);
-    }
-
-    match String::from_utf8(buffer) {
+    match encoder.encode_to_string(&metric_families) {
         Ok(metrics) => Ok(metrics),
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
