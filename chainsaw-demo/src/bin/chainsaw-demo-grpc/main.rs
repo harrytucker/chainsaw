27568b30f8a7096b75fe13ae973d39373aec5f17
Harry Tucker <harry.tucker@hpe.com>
2022-04-28 15:25:04 +0100

    Simplify grpc demo

--- chainsaw-demo/src/bin/chainsaw-demo-grpc/main.rs
+++ chainsaw-demo/src/bin/chainsaw-demo-grpc/main.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate tracing;
-
 use std::iter::once;
 
 use chainsaw::{config::get_configuration, Result};
@@ -24,12 +21,6 @@ async fn main() -> Result<()> {
     let subscriber = logging::new_subscriber(configuration.log.level)?;
     logging::set_global_logger(subscriber)?;
 
-    // TODO: Wrapping the function that returns these types in order to set the
-    // global serving status doesn't work due to some type-system shenanigans.
-    //
-    // Health service ergonomics may work better by instead initialising the
-    // HealthReporter and HealthServer within a Chainsaw::Server type than
-    // handling this in fn main().
     let (mut health_report, health_service) = tonic_health::server::health_reporter();
     health_report
         .set_serving::<GreeterServer<MyGreeter>>()
@@ -56,20 +47,11 @@ async fn main() -> Result<()> {
 
     // TODO: The gRPC server should be able to start up alongside other APIs,
     // such as extra gRPC services, HTTP endpoints, metrics etc.
-    info!(?addr, "Revving up Chainsaw!");
+    tracing::info!(?addr, "Revving up Chainsaw!");
     tokio::spawn(grpc);
 
-    match signal::ctrl_c().await {
-        Ok(()) => {
-            info!("Revving down Chainsaw...");
-            Ok(())
-        }
-        Err(err) => {
-            error!(
-                err = err.to_string().as_str(),
-                "Unable to listen for shutdown signal."
-            );
-            Err(err.into())
-        }
-    }
+    // Exit if SIGINT received.
+    signal::ctrl_c().await?;
+    tracing::info!("Revving down Chainsaw...");
+    Ok(())
 }
